6212af8bc4c7ffa675c63968863a94bb4afe1ed2
masuzakawataru <wataru.5756@gmail.com>
2024-08-13 11:51:45 +0900

    フッターの修正

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -1,25 +1,45 @@
 import Image from 'next/image';
 
-
 export function Footer() {
   return (
     <footer className="bg-gray-800 text-white py-8">
-      <div className="flex justify-between items-center container mx-auto px-5">
-      <a
-      href="https://x.com"
-      target="_blank"
-      rel="noopener noreferrer"
-      className="flex items-center space-x-2 hover:text-white"
-    >
-      <Image
-        src="/assets/x.svg"
-        alt="X"
-        width={24}
-        height={24}
-        className="text-gray-400 hover:text-white"
-      />
-    </a>
-        <p className="text-gray-400">© Ran</p>
+      <div className="container mx-auto px-5">
+        <div className="text-center text-2xl font-bold tracking-tighter">
+          Bon Voyage!
+        </div>
+        <div className="text-center mb-4">
+          <p className="text-gray-400">© Ran</p>
+        </div>
+        <div className="flex justify-center space-x-4">
+          <a
+            href="https://x.com/l10XZkPh8S24125"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:text-white"
+          >
+            <Image
+              src="/assets/x.svg"
+              alt="X"
+              width={24}
+              height={24}
+              className="text-gray-400 hover:text-white"
+            />
+          </a>
+          <a
+            href="https://github.com/Masurun"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:text-white"
+          >
+            <Image
+              src="/assets/github.svg"
+              alt="GitHub"
+              width={24}
+              height={24}
+              className=""
+            />
+          </a>
+        </div>
       </div>
     </footer>
   );
