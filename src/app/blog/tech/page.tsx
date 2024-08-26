3daff6893f8713049e98e9bf8c1264b5055336cb
Masurun <74701978+Masurun@users.noreply.github.com>
2024-08-26 22:12:56 +0900

    Update page.tsx

--- src/app/blog/tech/page.tsx
+++ src/app/blog/tech/page.tsx
@@ -1,16 +1,33 @@
-import Container from "@/app/_components/container";
-import { AllStories } from "@/app/_components/all-stories";
 import { getAllPosts } from "@/lib/api";
+import Container from "@/app/_components/container";
+import { Post } from "@/interfaces/post";
+import { PostPreview } from "@/app/_components/post-preview";
 
+export default async function Page() {
+  const allPosts: Post[] = await getAllPosts();
 
-export default function Page() {
-  const allPosts = getAllPosts();
   return (
     <main className="py-16">
       <Container>
+        <section>
+          <h2 className="mb-16 font-bold text-center text-4xl lg:text-5xl">
+            All Posts
+          </h2>
+
+          {/* Assuming there's no loading state for static generation */}
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-8 gap-y-16 mb-16">
+            {allPosts.map((post) => (
+              <PostPreview
+                key={post.slug}
+                title={post.title}
+                coverImage={post.coverImage}
+                date={post.date}
+                slug={post.slug}
+              />
+            ))}
+          </div>
+        </section>
       </Container>
     </main>
   );
 }
-
-
