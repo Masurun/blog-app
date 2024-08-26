d705b47bcfb693e901bec6190a40a61101d4674f
Masurun <74701978+Masurun@users.noreply.github.com>
2024-08-26 21:45:15 +0900

    Update page.tsx

--- src/app/blog/diary/page.tsx
+++ src/app/blog/diary/page.tsx
@@ -1,6 +1,5 @@
 import { getAllPosts } from "@/lib/api";
 import Container from "@/app/_components/container";
-import { AllStories } from "@/app/_components/all-stories";
 
 export default async function Page() {
   const allPosts = await getAllPosts();
