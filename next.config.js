bfd5e8638313b6e5c1e7e7fc316c6092c75cca87
Masurun <74701978+Masurun@users.noreply.github.com>
2024-08-31 22:53:52 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export"
-  images: {
-    unoptimized: true,
-  },
+  output: 'export',
 };
 
 module.exports = nextConfig;
