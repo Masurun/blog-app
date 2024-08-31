268f0b3f52650ce3d53bda5a2c5ee14cc85f69fe
masuzakawataru <wataru.5756@gmail.com>
2024-08-31 14:23:19 +0900

    お問い合わせページの修正

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,10 +1,26 @@
 // pages/contact.js
+import Container from "@/app/_components/container";
 
 export default function Contact() {
   return (
-    <div style={{ padding: "20px", fontFamily: "Arial, sans-serif" }}>
-      <h1>お問い合わせ</h1>
-      <p>お問い合わせは <a href="https://twitter.com/your_username" target="_blank" rel="noopener noreferrer">XのDM</a> までお願いします。</p>
-    </div>
+    <main className="py-16">
+      <Container>
+        <section>
+          <h2 className="mb-16 font-bold text-center text-4xl lg:text-5xl">
+            Contact
+          </h2>
+          <div className="flex justify-center">
+            <iframe
+              src="https://forms.gle/ByrgJQb6XnuqHg3c6"
+              width="100%"
+              height="600px"
+              frameBorder="0"
+              className="border-2 border-gray-300 rounded-lg"
+              allowFullScreen
+            ></iframe>
+          </div>
+        </section>
+      </Container>
+    </main>
   );
 }
