3882cd1fbc7380dbb41bad2b7809657967a01e0a
masuzakawataru <wataru.5756@gmail.com>
2024-09-01 11:53:08 +0900

    プロフィールページ編集

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,6 +1,7 @@
 import Container from "@/app/_components/container";
 import AnimatedContainers from "@/app/_components/animated-containers";
 import { ASSETS_PATH } from "@/lib/constants";
+import { PostPreview } from "@/app/_components/post-preview";
 
 export default function About() {
   return (
@@ -11,14 +12,16 @@ export default function About() {
           <div className="lg:w-1/2">
             <h2 className="mb-4 text-4xl font-bold lg:text-5xl">Profile</h2>
             <p className="mb-4 text-lg leading-relaxed">
-              Hi, I'm [Your Name], a [Your Profession] based in [Your Location].
-              I have a passion for [Your Interests or Hobbies]. With [X years]
-              of experience in [Your Field or Expertise], I specialize in [Your
-              Specialization].
+              神奈川県在住のWebエンジニアのらんです。
             </p>
-            <p className="text-lg leading-relaxed">
-              In my free time, I enjoy [Your Hobbies or Activities], and I'm
-              always eager to learn and explore new things.
+            <p className="mb-4 text-lg leading-relaxed">
+              主にPHP、JavaScript、Flutterでの開発に従事しています。
+            </p>
+            <p className="mb-4 text-lg leading-relaxed">
+              インフラ周りの知見が足りていないことを痛感し、今年中にAWS SAPとLPIC1の取得に向けて勉強中。
+            </p>
+            <p className="mb-4 text-lg leading-relaxed">
+              保有資格:	TOEIC800点 / 基本情報技術者 / AWS SAA / 簿記3級
             </p>
           </div>
           <div className="lg:w-1/2 flex justify-center lg:justify-end mb-8 lg:mb-0">
@@ -39,16 +42,16 @@ export default function About() {
           <h2 className="mb-8 text-3xl font-bold lg:text-4xl">Career</h2>
           <ul className="space-y-4">
             <li className="text-lg">
-              <span className="font-semibold">[Year Range]</span> - [Position]
-              at [Company]
+              <span className="font-semibold">2020年3月</span> - 獨協大学法学部法律学科を卒業
             </li>
             <li className="text-lg">
-              <span className="font-semibold">[Year Range]</span> - [Position]
-              at [Company]
+              <span className="font-semibold">2020年10月</span> - 新卒で小売業の会社に入社したが、半年で退職
             </li>
             <li className="text-lg">
-              <span className="font-semibold">[Year Range]</span> - [Position]
-              at [Company]
+              <span className="font-semibold">2020年11月</span> - プログラミングスクールにてRuby on Railsでのアプリ開発を学ぶ
+            </li>
+            <li className="text-lg">
+              <span className="font-semibold">2021年5月</span> - 渋谷のWeb制作会社に奇跡的に入社し今に至る
             </li>
           </ul>
         </section>
@@ -58,56 +61,14 @@ export default function About() {
       <AnimatedContainers>
         <section className="py-8">
           <h2 className="mb-8 text-3xl font-bold lg:text-4xl">Works</h2>
-          <div className="grid gap-8 md:grid-cols-2 lg:grid-cols-3">
-            <div className="p-4 bg-white rounded-lg shadow-md">
-              <img
-                src="/path/to/achievement-thumbnail1.jpg"
-                alt="Achievement 1"
-                className="w-full h-48 object-cover rounded-t-lg"
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-8 gap-y-16 mb-16">
+              <PostPreview
+                key={""}
+                title={"関連図作成アプリ"}
+                coverImage={"kanrenzu.png"}
+                date={"2024-08-31T00:00:00.000Z"}
+                slug={""}
               />
-              <div className="mt-4">
-                <h3 className="mb-2 text-xl font-semibold">
-                  Achievement Title 1
-                </h3>
-                <p className="text-gray-600">
-                  A brief description of the achievement, highlighting the key
-                  points and significance.
-                </p>
-              </div>
-            </div>
-            <div className="p-4 bg-white rounded-lg shadow-md">
-              <img
-                src="/path/to/achievement-thumbnail2.jpg"
-                alt="Achievement 2"
-                className="w-full h-48 object-cover rounded-t-lg"
-              />
-              <div className="mt-4">
-                <h3 className="mb-2 text-xl font-semibold">
-                  Achievement Title 2
-                </h3>
-                <p className="text-gray-600">
-                  A brief description of the achievement, highlighting the key
-                  points and significance.
-                </p>
-              </div>
-            </div>
-            <div className="p-4 bg-white rounded-lg shadow-md">
-              <img
-                src="/path/to/achievement-thumbnail3.jpg"
-                alt="Achievement 3"
-                className="w-full h-48 object-cover rounded-t-lg"
-              />
-              <div className="mt-4">
-                <h3 className="mb-2 text-xl font-semibold">
-                  Achievement Title 3
-                </h3>
-                <p className="text-gray-600">
-                  A brief description of the achievement, highlighting the key
-                  points and significance.
-                </p>
-              </div>
-            </div>
-            {/* Add more cards as needed */}
           </div>
         </section>
         </AnimatedContainers>
