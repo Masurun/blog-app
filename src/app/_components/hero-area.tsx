6e3d076f4689fef7780571aeae6d2552e7c295e8
masuzakawataru <wataru.5756@gmail.com>
2024-09-22 11:41:19 +0900

    元に戻す

--- src/app/_components/hero-area.tsx
+++ src/app/_components/hero-area.tsx
@@ -46,33 +46,33 @@ export default function HeroArea() {
 
   return (
     <section className="relative">
-    <div className="embla" ref={emblaRef}>
-      <div className="embla__container max-h-[420px] overflow-hidden">
-        {images.map((src, index) => (
-          <div key={index} className="embla__slide">
-            <Link href={links[index]} target="_blank" rel="noopener noreferrer">
+      <div className="embla" ref={emblaRef}>
+        <div className="embla__container max-h-[420px] overflow-hidden">
+          {images.map((src, index) => (
+            <div key={index} className="embla__slide">
+              <Link href={links[index]} target="_blank" rel="noopener noreferrer">
               <img
                 src={src}
                 alt={`Slide ${index + 1}`}
-                className="w-full h-full max-w-full max-h-[420px] object-cover"
+                className="h-full w-full object-cover"
               />
-            </Link>
-          </div>
+              </Link>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 flex space-x-2">
+        {images.map((_, index) => (
+          <button
+            key={index}
+            className={`w-3 h-3 rounded-full ${
+              index === currentIndex ? "bg-white" : "bg-gray-400"
+            }`}
+            onClick={() => goToSlide(index)}
+            aria-label={`Go to slide ${index + 1}`}
+          />
         ))}
       </div>
-    </div>
-    <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 flex space-x-2">
-      {images.map((_, index) => (
-        <button
-          key={index}
-          className={`w-3 h-3 rounded-full ${
-            index === currentIndex ? "bg-white" : "bg-gray-400"
-          }`}
-          onClick={() => goToSlide(index)}
-          aria-label={`Go to slide ${index + 1}`}
-        />
-      ))}
-    </div>
-  </section>
+    </section>
   );
 }
